d8fc790abf0a995ffda922d439a42078bc008475
Charles Ybarra <59151304+CharlieArray@users.noreply.github.com>
2020-11-19 18:47:05 -0600

    implemented add/delete card feature

--- src/App.js
+++ src/App.js
@@ -1,53 +1,121 @@
-import React, { Component } from 'react';
-import List from './List'
-import './App.css';
+import React from "react"
+import List from "./List"
+import "./App.css"
 
-class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
+const newRandomCard = () => {
+  const id = Math.random().toString(36).substring(2, 4)
+    + Math.random().toString(36).substring(2, 4);
+  return {
+    id,
+    title: `Random Card ${id}`,
+    content: 'lorem ipsum',
+  }
+}
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state= {
+      lists: [
+        {
+          id: '1',
+          header: 'First list',
+          cardIds: [ 'a', 'b', 'e', 'f', 'g', 'j', 'l', 'm' ],
+        },
+        {
+          id: '2',
+          header: 'Second list',
+          cardIds: ['b', 'c', 'd', 'f', 'h', 'i', 'k'],
+        },
+        {
+          id: '3',
+          header: 'Third list',
+          cardIds: [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' ],
+        },
+        {
+          id: '4',
+          header: 'Fourth list',
+          cardIds: [ 'l', 'm' ],
+        },
+      ],
+
+      allCards: {
+        'a': { id: 'a', title: 'First card', content: 'lorem ipsum' },
+        'b': { id: 'b', title: 'Second card', content: 'lorem ipsum' },
+        'c': { id: 'c', title: 'Third card', content: 'lorem ipsum' },
+        'd': { id: 'd', title: 'Fourth card', content: 'lorem ipsum' },
+        'e': { id: 'e', title: 'Fifth card', content: 'lorem ipsum' },
+        'f': { id: 'f', title: 'Sixth card', content: 'lorem ipsum' },
+        'g': { id: 'g', title: 'Seventh card', content: 'lorem ipsum' },
+        'h': { id: 'h', title: 'Eighth card', content: 'lorem ipsum' },
+        'i': { id: 'i', title: 'Ninth card', content: 'lorem ipsum' },
+        'j': { id: 'j', title: 'Tenth card', content: 'lorem ipsum' },
+        'k': { id: 'k', title: 'Eleventh card', content: 'lorem ipsum' },
+        'l': { id: 'l', title: 'Twelfth card', content: 'lorem ipsum' },
+        'm': { id: 'm', title: 'Thirteenth card', content: 'lorem ipsum' },
+      }
     }
-  };
+  
+  }
+
+  //make sure this event handler binds to the App (use arrow function) READ UP MORE ON BINDING
+  handleDeleteCard = (Card) => {
+    //console.log(Card);
+    const newLists = this.state.lists.map(list =>{
+      list.cardIds = list.cardIds.filter(
+        crd => crd !== Card
+      )
+      return list
+    })
+    this.setState({
+      lists: newLists
+    })
+  }
+
+   //make sure this event handler binds to the App (use arrow function) READ UP MORE ON BINDING
+  handleRandomCard = (listID) => {
+    const newCard = newRandomCard()
+    console.log(newCard)
+    
+    const newLists = this.state.lists.map(list =>{
+     if (list.id === listID){
+       list.cardIds.push(newCard.id)
+     }
+      return list
+      })
+    this.state.allCards[newCard.id] = newCard
+
+    this.setState({
+      lists: newLists,
+      allCards: this.state.allCards
+    })
+  }
 
   render() {
-    const { store } = this.props
+
+    //assign store to this.props to access props later 
+    const {lists, allCards} = this.state
+
     return (
-      <main className='App'>
-        <header className='App-header'>
+      <main className="App">
+        <header className="App-header">
           <h1>Trelloyes!</h1>
         </header>
-        <div className='App-list'>
-          {store.lists.map(list => (
-            <List
-              key={list.id}
-              header={list.header}
-              cards={list.cardIds.map(id => store.allCards[id])}
+        <div className="App-list">
+          {lists.map((list) => (
+            <List key={list.id} 
+            header={list.header} 
+            cards={list.cardIds.map((id) => allCards[id])}
+            onRandomItem = {this.handleRandomCard}
+            onDeleteItem = {this.handleDeleteCard}
+            id = {list.id}
             />
           ))}
         </div>
       </main>
-    );
+    )
   }
 }
 
-export default App;
-
-
-
-/*
-    The App component should render markup matching the design.html: the main element, a header with the h1 and a div with class 'App-list'.
-    The App component will accept 1 prop, store.
-        The store prop is an object with 2 keys: lists and allCards.
-        lists is an array of objects.
-        allCards is an object where each key is a card's ID and the value is the card object with a title and content.
-        You'll be given a dummy STORE object to pass to your App.
-    The App should render a List component for each of the items in the store.lists array.
-    Each instance of the List component should be passed 2 props (and a key). The 2 props are header and cards.
-        The header prop is a string for the List's header.
-        The cards prop will be an array of card objects.
-            You'll need to combine the cardIds array for the list with the allCards object.
-
-
-
-*/
+export default App
